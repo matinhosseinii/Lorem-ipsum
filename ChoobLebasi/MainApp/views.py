a00ebaebdcabca82fb3d4ddf63e8a1b94bbcdf3a
AMinWClub <aminsarkarwclub@gmail.com>
2024-01-04 21:57:51 +0330

    Initial Project

---
+++ ChoobLebasi/MainApp/views.py
@@ -0,0 +1,6 @@
+from django.shortcuts import render
+from django.http import HttpResponse, HttpResponseRedirect
+
+def Home(request):
+    return HttpResponse("be zoodi dar in makan choob lebasi nasb mishavad. ^_^")
+# Create your views here.
