4e987c5d10956519a82ce9da9731c43d1d07fb9b
AMinWClub <aminsarkarwclub@gmail.com>
2024-01-06 12:54:27 +0330

    Add Template Folder

--- ChoobLebasi/MainApp/views.py
+++ ChoobLebasi/MainApp/views.py
@@ -2,5 +2,5 @@ from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseRedirect
 
 def Home(request):
-    return HttpResponse("be zoodi dar in makan choob lebasi nasb mishavad. ^_^")
+    return render(request, "MainApp/PasteHtmlFilesHere.html")
 # Create your views here.
